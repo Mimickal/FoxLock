20e8d1ecd2e22b3b832560da98c256d8df06a22f
Mia Moretti <mimickal.dev@gmail.com>
2017-12-24 22:45:29 -0500

    Hard-coded key can be retrieved via GET

--- foxlock.py
+++ foxlock.py
@@ -2,6 +2,13 @@ from flask import Flask, request
 
 app = Flask('foxlock');
 
+@app.route('/key/', methods=['GET'])
+def keyRoute():
+	'''This is a proof of concept, so we're going to violate boundaries for a little while.'''
+	userkey = open('resources/userkey.txt', 'r').read()
+	return userkey
+
+
 if __name__ == '__main__':
     context = ('resources/cert.pem', 'resources/key.pem')
     app.run(ssl_context=context)
