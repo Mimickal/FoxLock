88a9c62995bbedeeb5c119385283310cbbe5104b
Mia Moretti <mimickal.dev@gmail.com>
2017-12-27 17:10:50 -0500

    Decode JWT and return hardcoded key

--- foxlock.py
+++ foxlock.py
@@ -1,15 +1,20 @@
 from flask import Flask, request
+import jwt
 
 app = Flask('foxlock');
 
-@app.route('/key/', methods=['GET'])
+@app.route('/key', methods=['GET'])
 def keyRoute():
 	'''This is a proof of concept, so we're going to violate boundaries for a little while.'''
-	userkey = open('resources/userkey.txt', 'r').read()
-	return userkey
+	encodedToken = request.args.get('token')
+	userPublicKey = open('tempdevstuff/testkey.pub', 'r').read()
+
+	data = jwt.decode(encodedToken, userPublicKey, algorithm='RS256')
+
+	requestedkey = open('resources/userkey.txt', 'r').read()
+	return requestedkey
 
 
 if __name__ == '__main__':
     context = ('resources/cert.pem', 'resources/key.pem')
     app.run(ssl_context=context)
-
