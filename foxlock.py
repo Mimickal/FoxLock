c79356f57dea4ba80f4cba23b143990145806669
Mia Moretti <mimickal.dev@gmail.com>
2018-01-06 02:59:13 -0500

    Client name is now specified when getting keys

--- foxlock.py
+++ foxlock.py
@@ -1,17 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 import jwt
+import re
 
 app = Flask('foxlock');
 
-@app.route('/key', methods=['GET'])
-def keyRoute():
+@app.route('/key/<client>', methods=['GET'])
+def keyRoute(client):
 	'''This is a proof of concept, so we're going to violate boundaries for a little while.'''
+	# Client may only have alpha-numeric names
+	if re.search('[^a-zA-Z0-9]', client):
+		abort(400)
+
 	encodedToken = request.args.get('token')
-	userPublicKey = open('keys/testuser/key_public.rsa', 'r').read()
-
+	userPublicKey = open('keys/' + client + '/key_public.rsa', 'r').read()
 	data = jwt.decode(encodedToken, userPublicKey, algorithm='RS256')
+	requestedkey = open('keys/' + client + '/testkey.key', 'r').read()
 
-	requestedkey = open('keys/testuser/testkey.key', 'r').read()
 	return requestedkey
 
 
