0159d6f39d327d0f3fc3d9f6d5beedf118ab05b1
Mia Moretti <mimickal.dev@gmail.com>
2018-01-14 04:55:27 -0500

    Move shared functionality into helper functions
    This is in preparation for addKey

--- impl.py
+++ impl.py
@@ -3,27 +3,11 @@ import jwt
 import re
 
 def getKey(client):
-	# Client may only have alpha-numeric names
-	if re.search('[^a-zA-Z0-9]', client):
-		abort(400)
+	validateClientName(client)
 
-	encoded_token = request.args.get('token')
-	if encoded_token is None:
-		abort(400)
-
-	# A client exists in our system if there is a matching key directory
-	try:
-		client_rsa_public_key = open('keys/' + client + '/key_rsa.pub', 'r').read()
-	except IOError:
-		abort(404) # Client public key not found
-
-	# Most JWT errors will come from clients signing JWTs with the wrong key
-	try:
-		client_request = jwt.decode(encoded_token, client_rsa_public_key, algorithm='RS256')
-	except jwt.exceptions.DecodeError:
-		abort(400) # Client's key might not be right, or they're not utf-8 decoding their JWT
-	except jwt.exceptions.InvalidTokenError:
-		abort(400) # JWT is malformed
+	encoded_token = retrieveToken()
+	client_rsa_public_key = loadClientRSAKey(client)
+	client_request = decodeToken(encoded_token, client_rsa_public_key)
 
 	# Keys may only have alpha-numeric names
 	try:
@@ -44,4 +28,35 @@ def getKey(client):
 def getJwtKey():
 	server_jwt_rsa_public_key = open('resources/jwt_key.pub', 'r').read()
 	return server_jwt_rsa_public_key
+
+##################
+# Helper Functions
+##################
+
+def validateClientName(name):
+	"""Client may only have alpha-numeric names"""
+	if re.search('[^a-zA-Z0-9]', name):
+		abort(400)
+
+def retrieveToken():
+	token = request.args.get('token')
+	if token is None:
+		abort(400)
+	return token
+
+def loadClientRSAKey(client):
+	"""A client exists in our system if there is a matching key directory"""
+	try:
+		return open('keys/' + client + '/key_rsa.pub', 'r').read()
+	except IOError:
+		abort(404) # Client public key not found
+
+def decodeToken(token, public_key):
+	"""Most JWT errors will come from clients signing JWTs with the wrong key"""
+	try:
+		return jwt.decode(token, public_key, algorithm='RS256')
+	except jwt.exceptions.DecodeError:
+		abort(400) # Client's key might not be right, or they're not utf-8 decoding their JWT
+	except jwt.exceptions.InvalidTokenError:
+		abort(400) # JWT is malformed
 
