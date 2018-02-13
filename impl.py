71896344d7e053096c07651fcd93e1af299aefd7
Mia Moretti <mimickal.dev@gmail.com>
2018-02-13 07:20:16 -0500

    Rename and refactor some stuff

--- impl.py
+++ impl.py
@@ -13,21 +13,23 @@ def getKey(client):
 	"""
 	global SERVER_JWT_PRIVATE_KEY
 
-	client_request = decodeRequestToken(client)
+	validateClient(client)
+
+	client_pub_key = loadClientRSAKey(client)
+	token_data = decodeRequestToken(request.args.get('token'), client_pub_key)
 
 	# Keys may only have alpha-numeric names
 	try:
-		if re.search('[^a-zA-Z0-9]', client_request['key']):
+		if re.search('[^a-zA-Z0-9]', token_data['key']):
 			abort(400) # Invalid key requested
-		requested_key = open('keys/%s/%s.key' % (client, client_request['key']), 'r').read()
+		requested_key = open('keys/%s/%s.key' % (client, token_data['key']), 'r').read()
 	except KeyError:
 		abort(400) # JWT did not contain key
 	except IOError:
 		abort(404) # Key not found
 
-	# Key is returned in an RSA256 signed JWT so client can be sure it actually came from us
-	server_jwt_rsa_private_key = open('resources/jwt_key', 'r').read()
-	keytoken = jwt.encode({'key': requested_key}, SERVER_JWT_PRIVATE_KEY, algorithm='RS256')
+	# Key is returned in a JWT encrypted with the client's public key, so only they can decrypt it
+	keytoken = packJWT({'key': requested_key}, SERVER_JWT_PRIVATE_KEY)
 
 	return keytoken.decode('utf-8')
 
@@ -35,7 +37,10 @@ def addKey(client):
 	"""Adds a new key with the specified name and contents.
 	Returns an error if a key with the specified name already exists.
 	"""
-	token_data = decodeRequestToken(client)
+	validateClient(client)
+
+	client_pub_key = loadClientRSAKey(client)
+	token_data = decodeRequestToken(request.args.get('token'), client_pub_key)
 	validateNewKeyData(token_data)
 
 	# Use 'x' flag so we can throw an error if a key with this name already exists
@@ -51,7 +56,10 @@ def updateKey(client):
 	"""Updates the contents of a key that already exists in our system.
 	Returns an error if the specified key doesn't exist for the specified user.
 	"""
-	token_data = decodeRequestToken(client)
+	validateClient(client)
+
+	client_pub_key = loadClientRSAKey(client)
+	token_data = decodeRequestToken(request.args.get('token'), client_pub_key)
 	validateNewKeyData(token_data)
 
 	# Use 'w' flag to replace existing key file with the new key data
@@ -72,42 +80,36 @@ def getJwtKey():
 # Helper Functions
 ##################
 
-def decodeRequestToken(client):
-	"""Decodes the request's JWT with the specified client's RSA public key (RS256).
-	Returns an error if:
-		- The specified client doesn't exist
-		- Their RSA public key isn't in our system
-		- The token isn't provided in the request
-		- There's an issue decoding the JWT
-	"""
+def validateClient(client):
 	# Client may only have alpha-numeric names
 	if re.search('[^a-zA-Z0-9]', client):
 		abort(400)
-
-	# Does the client even exist in our system?
 	if not os.path.isdir('keys/' + client):
 		abort(404) # Client doesn't exist
 
+def loadClientRSAKey(client):
+	"""Load a client's RSA public key, if they exist in our system"""
+	try:
+		key = open('keys/%s/key_rsa.pub' % client, 'rb').read()
+	except IOError:
+		abort(404) # Client public key not found
+	return key
+
+def decodeRequestToken(token, client_pub_key):
+	"""Decrypts / decodes the request's JWT with the server's JWT private key."""
 	# Flask keeps track of the current request information in its built-in request object
 	token = request.args.get('token')
 	if token is None:
 		abort(400)
 
-	# A client exists in our system if there is a matching key directory
-	try:
-		client_rsa_public_key = open('keys/' + client + '/key_rsa.pub', 'r').read()
-	except IOError:
-		abort(404) # Client public key not found
-
 	# Most JWT errors will come from clients signing JWTs with the wrong key
 	try:
-		decoded_token = jwt.decode(token, client_rsa_public_key, algorithm='RS256')
+		decoded_token_data = unpackJWT(token, client_pub_key)
 	except jwt.exceptions.DecodeError:
 		abort(400) # Client's key might not be right, or they're not utf-8 decoding their JWT
 	except jwt.exceptions.InvalidTokenError:
 		abort(400) # JWT is malformed
-
-	return decoded_token
+	return decoded_token_data
 
 def validateNewKeyData(data):
 	"""Verify that the client provided a key name and key data in their request"""
@@ -116,4 +118,15 @@ def validateNewKeyData(data):
 		data['key']
 	except KeyError:
 		abort(400) # Token data must include 'key' and 'name'
+
+
+# We've switched JWT libraries 3 times in one week, so let's just wrap JWT functionality
+
+def packJWT(data, key):
+	"""Encrypt/encode in a compact statement"""
+	return jwt.encode(data, key, algorithm='RS256')
+
+def unpackJWT(encoded_jwt, key):
+	"""Decode/Decrypt in a compact statement"""
+	return jwt.decode(encoded_jwt, key, algorithm='RS256')
 
