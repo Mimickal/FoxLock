ac78fc744c958f3b84c1e5c404317c11897291a2
Mia Moretti <mimickal.dev@gmail.com>
2018-02-13 20:36:30 -0500

    Handle AES key internally rather than making clients do it

--- HybridRSA.py
+++ HybridRSA.py
@@ -3,6 +3,7 @@ from Crypto.Cipher import PKCS1_OAEP, AES
 from Crypto.Hash import SHA256
 from Crypto import Random
 import os
+import math
 
 '''This module simplifies encrypting arbitrarily large data using RSA.
 
@@ -64,9 +65,8 @@ def encryptRSA(byte_string, key):
 	cipher = PKCS1_OAEP.new(rsa_key, hashAlgo=SHA256.new())
 	return cipher.encrypt(byte_string)
 
-def decryptRSA(encrypted, key):
+def decryptRSA(encrypted, rsa_key):
 	'''Decrypts a PKCS1 OAEP encrypted string'''
-	rsa_key = RSA.importKey(key)
 	cipher = PKCS1_OAEP.new(rsa_key, hashAlgo=SHA256.new())
 	return cipher.decrypt(encrypted)
 
@@ -81,14 +81,15 @@ def encrypt(string, rsa_key):
 	aes_key = os.urandom(int(AES_KEY_SIZE / 8))
 	enc_string = encryptAES(string, aes_key)
 	enc_key = encryptRSA(aes_key, rsa_key)
-	return {
-		'key': enc_key,
-		'data': enc_string
-	}
+	return enc_key + enc_string
 
 def decrypt(encrypted, rsa_key):
 	'''Decrypt RSA/AES hybrid encrypted data'''
-	aes_key = decryptRSA(encrypted['key'], rsa_key)
-	decrypted_data = decryptAES(encrypted['data'], aes_key)
+	# Import key here because we need the key size to extract the AES key
+	rsa_key_obj = RSA.importKey(rsa_key)
+	key_size = math.ceil(math.log(rsa_key_obj.n, 2)) // 8
+
+	aes_key = decryptRSA(encrypted[:key_size], rsa_key_obj)
+	decrypted_data = decryptAES(encrypted[key_size:], aes_key)
 	return decrypted_data
 
