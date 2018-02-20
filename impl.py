3cbc21223a4d50e2ed9e137e5fecec4238f466e1
Mia Moretti <mimickal.dev@gmail.com>
2018-02-20 02:43:49 -0500

    Use HybridRSA and Base64 encoding for packing data, and pass token in Authorization header

--- impl.py
+++ impl.py
@@ -2,8 +2,10 @@ from flask import request
 import jwt
 import re
 import os
+from base64 import b64encode, b64decode
 
 from errors import BadRequest, NotFound
+import HybridRSA
 
 # Cache server keys because they don't change during program operation
 SERVER_JWT_PRIVATE_KEY = open('resources/jwt_key', 'rb').read()
@@ -18,7 +20,7 @@ def getKey(client):
 	validateClient(client)
 
 	client_pub_key = loadClientRSAKey(client)
-	token_data = decodeRequestToken(request.args.get('token'), client_pub_key)
+	token_data = decodeRequestToken(request.headers['Authorization'], client_pub_key)
 
 	# Keys may only have alpha-numeric names
 	try:
@@ -31,9 +33,9 @@ def getKey(client):
 		raise BadRequest("Key '%s' not found" % token_data['key'])
 
 	# Key is returned in a JWT encrypted with the client's public key, so only they can decrypt it
-	keytoken = packJWT({'key': requested_key}, SERVER_JWT_PRIVATE_KEY)
+	keytoken = packJWT({'key': requested_key}, SERVER_JWT_PRIVATE_KEY, client_pub_key)
 
-	return keytoken.decode('utf-8')
+	return keytoken
 
 def addKey(client):
 	"""Adds a new key with the specified name and contents.
@@ -42,7 +44,7 @@ def addKey(client):
 	validateClient(client)
 
 	client_pub_key = loadClientRSAKey(client)
-	token_data = decodeRequestToken(request.args.get('token'), client_pub_key)
+	token_data = decodeRequestToken(request.headers['Authorization'], client_pub_key)
 	validateNewKeyData(token_data)
 
 	# Use 'x' flag so we can throw an error if a key with this name already exists
@@ -61,7 +63,7 @@ def updateKey(client):
 	validateClient(client)
 
 	client_pub_key = loadClientRSAKey(client)
-	token_data = decodeRequestToken(request.args.get('token'), client_pub_key)
+	token_data = decodeRequestToken(request.headers['Authorization'], client_pub_key)
 	validateNewKeyData(token_data)
 
 	# Use 'w' flag to replace existing key file with the new key data
@@ -96,14 +98,18 @@ def loadClientRSAKey(client):
 		raise NotFound('Client RSA public key not found')
 	return key
 
-def decodeRequestToken(token, client_pub_key):
+def decodeRequestToken(auth_header, client_pub_key):
 	"""Decrypts / decodes the request's JWT with the server's JWT private key."""
-	if token is None:
+	global SERVER_JWT_PRIVATE_KEY
+
+	if auth_header is None:
 		raise BadRequest('No token found in request')
+
+	token = auth_header.lstrip('Bearer ')
 
 	# Most JWT errors will come from clients signing JWTs with the wrong key
 	try:
-		decoded_token_data = unpackJWT(token, client_pub_key)
+		decoded_token_data = unpackJWT(token, client_pub_key, SERVER_JWT_PRIVATE_KEY)
 	except jwt.exceptions.DecodeError:
 		raise BadRequest('Failed to decode JWT. Are you using the right key?')
 	except jwt.exceptions.InvalidTokenError:
@@ -121,11 +127,16 @@ def validateNewKeyData(data):
 
 # We've switched JWT libraries 3 times in one week, so let's just wrap JWT functionality
 
-def packJWT(data, key):
+def packJWT(data, sign_key, encrypt_key):
 	"""Encrypt/encode in a compact statement"""
-	return jwt.encode(data, key, algorithm='RS256')
+	token = jwt.encode(data, sign_key, algorithm='RS256')
+	enc_token = HybridRSA.encrypt(token, encrypt_key)
+	return b64encode(enc_token).decode('utf-8')
 
-def unpackJWT(encoded_jwt, key):
+def unpackJWT(encoded_jwt, verify_key, decrypt_key):
 	"""Decode/Decrypt in a compact statement"""
-	return jwt.decode(encoded_jwt, key, algorithm='RS256')
+	decoded = b64decode(encoded_jwt)
+	dec_token = HybridRSA.decrypt(decoded, decrypt_key)
+	token = jwt.decode(dec_token, verify_key, algorithm='RS256')
+	return token
 
